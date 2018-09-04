fc51f2b6d0cbfc2bac8fbfba912c461f371e827e
Swagnik Dutta <swagnikd@gmail.com>
2018-09-04 16:13:12 +0530

    css changes minor

--- src/components/Collections/Card/Style.js
+++ src/components/Collections/Card/Style.js
@@ -7,12 +7,19 @@ export const CardWrapper = styled.div`
 		float: left;
 		margin-right: 2.5%;
 		margin-bottom: 2.5%;
-		box-shadow: 2px 2px 10px lightgrey;
+		box-shadow: 2px 2px 10px #E3E3E3;
 		position: relative;
 		box-sizing: border-box;
+		cursor: pointer;
+		transition: box-shadow 500ms;
+		border-radius: 4px;
 	}
 	&:nth-child(3n+1){
 		margin-right: 0;
+	}
+	&:hover{
+		box-shadow: 1px 1px 1px #E3E3E3;
+		transition: box-shadow 500ms;
 	}
 `;
 
@@ -23,7 +30,8 @@ export const CardImage = styled.div`
 	left: 0;
 	box-sizing: border-box;
 	background: url(${props => props.imageLink});
-	background-size: contain;
+	background-size: cover;
+	border-radius: 4px 0 0 4px;
 `;
 
 export const CardDetails = styled.div`
