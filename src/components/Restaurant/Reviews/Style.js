4b0a8df6cbc4836c91c5c2fbdc48cb6d28b62c00
Swagnik Dutta <swagnikd@gmail.com>
2018-09-24 04:52:48 +0530

    simple css change that made all the difference

--- src/components/Restaurant/Reviews/Style.js
+++ src/components/Restaurant/Reviews/Style.js
@@ -4,8 +4,5 @@ export const Wrapper = styled.div`
 	width: 68%;
 	float: left;
 	padding: 20px;
-	box-shadow: 2px 2px 30px #C8C8C8;
-	border-right: 1px solid #C8C8C8;
-	border-bottom: 1px solid #C8C8C8;
-	border-radius: 4px;
+	border-left: 1px solid #C8C8C8;
 `;
