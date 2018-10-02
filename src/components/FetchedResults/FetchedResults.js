4dcc0c3f900358d1be8155eb2c2324a0c6d8bcae
Swagnik Dutta <swagnikd@gmail.com>
2018-10-02 19:57:16 +0530

    completed data part of card component

--- src/components/FetchedResults/FetchedResults.js
+++ src/components/FetchedResults/FetchedResults.js
@@ -4,18 +4,44 @@ import _ from 'lodash';
 // components
 import Card from './Card/Card';
 
-// import { Wrapper } from './Style.js';
+// config
+import { restaurant as restaurantConfig } from '../../config/SearchResultsConfig';
+
+const getRestaurantCardComponent = (restaurantObj, idx) => {
+	let temp = {};
+	
+	Object.keys(restaurantConfig).forEach((elem, idx) => {
+		let value = _.get(restaurantObj, restaurantConfig[elem].path, '');
+
+		if(restaurantConfig[elem].extraData){
+			restaurantConfig[elem].extraData.forEach((ele) => {
+				if(ele.insertAt === 'end'){
+					value += ele.message ? ele.message: ` ${_.get(restaurantObj, ele.path, '')}`;
+				} else if(ele.insertAt === 'start'){
+					value = (ele.message ? ele.message : _.get(restaurantObj, ele.path, '')) + ` ${value}`;
+				}
+			});
+		}
+		temp[elem] = value;
+	});
+
+	return <Card key={idx}
+				name={temp.name}
+				aggregate_rating={temp.aggregate_rating}
+				rating_color={temp.rating_color}
+				votes={temp.votes}
+				address_short={temp.address_short}
+				address_long={temp.address_long}
+				cuisines={temp.cuisines}
+				average_cost_for_two={temp.average_cost_for_two} />
+}
 
 const fetchedResults = (props) => {
-
-	let restaurantCollections = props.restaurants.map((elem, idx) => {
-				
-		return <Card key={idx} />
-	});
+	let restaurantsList = props.restaurants.map((elem, idx) => getRestaurantCardComponent(elem.restaurant, idx));
 
 	return(
 		<div>
-			{restaurantCollections}
+			{restaurantsList}
 		</div>
 	);
 };
---
+++ src/config/SearchResultsConfig.js
@@ -0,0 +1,41 @@
+const masterConfig = {
+	restaurant: {
+		name: {
+			path: 'name',
+		},
+		aggregate_rating: {
+			path: 'user_rating.aggregate_rating',
+		},
+		rating_color: {
+			path: 'user_rating.rating_color',
+		},
+		votes: {
+			path: 'user_rating.votes',
+		},
+		address_short: {
+			path: 'location.locality_verbose',
+		},
+		address_long: {
+			path: 'location.address',
+		},
+		cuisines: {
+			path: 'cuisines',
+		},
+		average_cost_for_two: {
+			path: 'average_cost_for_two',
+			extraData: [{
+				insertAt: 'start',
+				path: 'restaurant.currency',
+			}]
+		}
+	},
+	// filters: {
+	// 	rating: {
+	// 		path: 'rating'
+	// 	}
+	// }
+};
+
+module.exports = {
+	...masterConfig
+}
