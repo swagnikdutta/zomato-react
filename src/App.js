2071bbf82b275fef24c809e2a5c656dc17b87da7
Swagnik Dutta <swagnikd@gmail.com>
2018-10-23 02:38:07 +0530

    minor changes, removing warnings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import _ from 'lodash';
 
 import './App.css';
 
