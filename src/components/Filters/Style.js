4dd0077e32a32cc8abaef7280183d089af6dae83
Swagnik Dutta <swagnikd@gmail.com>
2018-10-25 03:49:17 +0530

    checking if media query works with style component on prod heroku

--- src/components/Filters/Style.js
+++ src/components/Filters/Style.js
@@ -4,4 +4,8 @@ export const Wrapper = styled.div`
 	padding: 20px;
 	border-radius: 8px;
 	border: 1px solid lightgrey;
+
+	@media (min-width: 320px) and (max-width: 480px) {
+		display: none;
+	}
 `;
